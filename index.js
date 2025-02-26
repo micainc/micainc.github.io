e8d96208841f6babd63a105201c6599e38a04b55
kfilyk <kelvinfilyk@gmail.com>
2025-02-25 16:46:17 -0800

    Added feature fix where sample height is 512 for widescreen/desktop, and 256px for phone/mobile

--- index.js
+++ index.js
@@ -396,6 +396,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         });
+
+        // Add a handler to reset the layout when scrolling stops
+        let scrollTimeout;
+        window.addEventListener('scroll', function() {
+            clearTimeout(scrollTimeout);
+            scrollTimeout = setTimeout(updateLayout, 200);
+        });
     }
 
     // Call this function to set up touch events
